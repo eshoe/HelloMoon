ee62d44997529c94a43f711800ea5113c9b4292f
Erin Schumacher <eschumacher@twitter.com>
2015-07-09 16:50:26 -0700

    Adding audio

--- app/src/main/java/com/erinworks/hellomoon/HelloMoonActivity.java
+++ app/src/main/java/com/erinworks/hellomoon/HelloMoonActivity.java
@@ -10,6 +10,13 @@ public class HelloMoonActivity extends Activity {
     Button playButton;
     Button stopButton;
 
+    AudioPlayer player = new AudioPlayer();
+
+    void updateButtons() {
+        boolean isEnabled = !player.isPlaying();
+        playButton.setEnabled(isEnabled);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +27,25 @@ public class HelloMoonActivity extends Activity {
 
         playButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(HelloMoonActivity.this, R.string.hellomoon_toast_play, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(HelloMoonActivity.this, R.string.hellomoon_toast_play, Toast.LENGTH_SHORT).show();
+                player.play(HelloMoonActivity.this);
+                updateButtons();
             }
         });
 
         stopButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(HelloMoonActivity.this, R.string.hellomoon_toast_stop, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(HelloMoonActivity.this, R.string.hellomoon_toast_stop, Toast.LENGTH_SHORT).show();
+                player.stop();
+                updateButtons();
             }
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
     }
 
 }
