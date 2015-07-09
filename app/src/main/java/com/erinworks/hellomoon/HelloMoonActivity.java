a60d0040c98458d2be77043312cc1def3bbdbb5e
Erin Schumacher <eschumacher@twitter.com>
2015-07-09 16:18:16 -0700

    Creating a caption and setting image file in code.

--- app/src/main/java/com/erinworks/hellomoon/HelloMoonActivity.java
+++ app/src/main/java/com/erinworks/hellomoon/HelloMoonActivity.java
@@ -5,16 +5,22 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ImageView;
 
 public class HelloMoonActivity extends Activity {
     Button playButton;
     Button stopButton;
+    ImageView image;
+    int picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_moon);
 
+        picture = R.mipmap.armstrong_on_moon;
+        image = (ImageView)findViewById(R.id.hellomoon_image);
+        image.setImageResource(picture);
         playButton = (Button)findViewById(R.id.hellomoon_playButton);
         stopButton = (Button)findViewById(R.id.hellomoon_stopButton);
 
@@ -29,6 +35,14 @@ public class HelloMoonActivity extends Activity {
                 Toast.makeText(HelloMoonActivity.this, R.string.hellomoon_toast_stop, Toast.LENGTH_SHORT).show();
             }
         });
+
+        image.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(HelloMoonActivity.this, R.string.hellomoon_image_description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 }
